e21f2cd4d85d54114e75905a75923c1139aab883
Leroux Hugo <tourniole76@gmail.com>
2024-07-30 10:47:23 +0200

    [MOD] Security Update

--- backend/taktor.py
+++ backend/taktor.py
@@ -2,8 +2,7 @@ import subprocess
 
 def check_rule_exists(ip):
     try:
-        commande = f'netsh advfirewall firewall show rule name="Block {ip}"'
-        result = subprocess.run(commande, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        result = subprocess.run(f'netsh advfirewall firewall show rule name="Block {ip}"', shell=True, check=True, capture_output=True)
         return "No rules match the specified criteria" not in result.stdout.decode()
     except subprocess.CalledProcessError as e:
         return False
@@ -13,16 +12,14 @@ def block_ip(ip):
         print(f"A rule already exists for IP {ip}.")
         return
     try:
-        commande = f'netsh advfirewall firewall add rule name="Block {ip}" dir=in action=block remoteip={ip}'
-        subprocess.run(commande, shell=True, check=True)
+        subprocess.run(f'netsh advfirewall firewall add rule name="Block {ip}" dir=in action=block enable=yes remoteip={ip}', shell=True, check=True)
         print(f"IP {ip} block with success.")
     except subprocess.CalledProcessError as e:
         print(f"Error while blocking IP {ip}: {e}")
 
 def unblock_ip(ip):
     try:
-        commande = f'netsh advfirewall firewall delete rule name="Block {ip}" remoteip={ip}'
-        subprocess.run(commande, shell=True, check=True)
+        subprocess.run(f'netsh advfirewall firewall delete rule name="Block {ip}"', shell=True, check=True)
         print(f"IP {ip} unblock with success.")
     except subprocess.CalledProcessError as e:
         print(f"Error while unblocking IP {ip}: {e}")
