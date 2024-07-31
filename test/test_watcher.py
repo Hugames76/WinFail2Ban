1b3993a26749b79bd11f5086a831ee40aa4b54a0
Leroux Hugo <tourniole76@gmail.com>
2024-07-31 09:22:26 +0200

    [ADD] __init__ and adapt test_watcher

--- test/test_watcher.py
+++ test/test_watcher.py
@@ -5,14 +5,18 @@ def test_set_watcher_config():
     result = set_watcher_config(5, 10, {"192.168.1.1", "192.168.1.2"})
     assert result == (5, 10, {"192.168.1.1", "192.168.1.2"})
 
-def test_handle_total_attempt():
-    total_attempts.clear()
-    print(handle_total_attempt('192.168.1.1'))
-    assert total_attempts['192.168.1.1'] == 1
+def test_handle_total_attempt(mocker):
+    ip = "192.168.1.1"
+    mock_total_attempts = mocker.patch('backend.watcher.total_attempts', {ip: 10}, create=True)
+    mock_requests_post = mocker.patch('requests.post')
+    handle_total_attempt(ip)
+    assert mock_total_attempts[ip] == 11
+    mock_requests_post.assert_called_with(f"http://127.0.0.1:5000/log_attempt/{ip}", json={"is_failed": False})
 
-def test_handle_failed_attempt():
-    failed_attempts.clear()
-    total_attempts.clear()
-    handle_failed_attempt('192.168.1.1')
-    assert failed_attempts['192.168.1.1'] == 1
-    assert total_attempts['192.168.1.1'] == 1
+def test_handle_failed_attempt(mocker):
+    ip = "192.168.1.1"
+    mock_failed_attempt = mocker.patch('backend.watcher.failed_attempts', {ip: 2}, create=False)
+    mock_requests_post = mocker.patch('requests.post')
+    handle_failed_attempt(ip)
+    assert mock_failed_attempt[ip] == 3
+    mock_requests_post.assert_called_with(f"http://127.0.0.1:5000/log_attempt/{ip}", json={"is_failed": True})
